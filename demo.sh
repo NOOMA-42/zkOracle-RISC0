d4815b8d4292350cf5435cb5870d65f06e09735c
nooma <suncloud2203357667@gmail.com>
2023-11-12 00:37:52 +0300

    add get request in bonsai

---
+++ demo.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+fuser 3000/tcp
+
+# Check if at least one argument was provided
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <bonsai> <devmode>..."
+    echo "Argument should be either T or F"
+    exit 1
+fi
+
+# Bonsai or not
+if [ "$1" = "T" ]; then
+    export BONSAI_API_URL="https://api.bonsai.xyz/"
+    export BONSAI_API_KEY="AdiM1hmFnyaNMt3dZQPSr6xMfiZvISQZ6hQASSTt"
+elif [ "$1" = "F" ]; then
+    export BONSAI_API_URL=""
+    export BONSAI_API_KEY=""
+else
+    echo "error: invalid argument: $1"
+    exit 
+fi
+
+# Devmode or not
+if [ "$2" = "T" ]; then
+    export DEVMODE=true
+elif [ "$2" = "F" ]; then
+    export DEVMODE=false
+else
+    echo "error: invalid argument: $1"
+    exit
+fi
+
+# Setup
+cd simple-proxy
+yarn 
+
+# Init simple-proxy server
+yarn start &
+
+# Execute oracle
+cd ..
+cargo run
